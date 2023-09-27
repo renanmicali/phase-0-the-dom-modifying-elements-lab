3b267759dbf2256f0fd0949d4972ca409c0c4ce9
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-09-27 18:50:49 +1000

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const element1 = document.getElementById("main")
+element1.remove();
+
+const newHeader = document.createElement("h1")
+
+// For some reason this code was getting in the way 
+// const element2 = document.getElementById("body")
+// element2.innerHTML = newHeader
+
+newHeader.setAttribute("id","victory")
+
+newHeader.innerHTML = "RKM is the champion";
